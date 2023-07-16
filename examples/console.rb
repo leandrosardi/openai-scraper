af1cbfb1aac51a59b14c5c600b4704fc045f36e9
leandrosardi <leandro.sardi@expandedventure.com>
2023-07-16 11:47:27 -0300

    coding

--- examples/console.rb
+++ examples/console.rb
@@ -1,7 +1,6 @@
 require_relative '../lib/openai-scraper.rb'
 require_relative '../config.rb'
 
-filename = './hgwarchitecture.html'
 l = BlackStack::LocalLogger.new('console.log')
 
 l.log BlackStack::OpenAIScraper::NAME.green
@@ -10,53 +9,10 @@ l.logs "Initializing... "
 BlackStack::OpenAIScraper.init
 l.logf "done".green
 
-while true
-    prompt = nil
-    print "#{BlackStack::OpenAIScraper::PROMPT}> ".blue
-    s = gets.chomp
-    begin
-        # `\q` to quit
-        if s == '\q'
-            exit(0)
-        # `\h` for help
-        elsif s == '\h'
-            puts BlackStack::OpenAIScraper.help 
-            next
-        # \wl <url>: download the web-page and pass the list of links to the model for further reference.\n
-        elsif s =~ /\\wl/
-            # find the url after the `\wl`, when \wl may be at any position into the string
-            prompt = s
-            i = 0
-            s.split(' ').each { |x|
-                if x == '\wl'
-                    url = s.split(' ')[i+1]
-                    prompt.gsub!(/\\wl #{url}/, BlackStack::OpenAIScraper.wl(url).to_s)
-                end
-                i += 1
-            }
-            puts BlackStack::OpenAIScraper::HINT1
-            puts prompt.blue
-        # \wt <url>: download the web-page and pass the text content to the model for further reference.\n 
-        elsif s.start_with?('\wt')
-            raise 'Not implemented yet'
-        else 
-            prompt = s
-        end
-binding.pry
-        # standard openai prompt
-        puts BlackStack::OpenAIScraper.response(prompt).to_s.green
+# launch the console
+l.log "Launching console:"
+BlackStack::OpenAIScraper.console
 
-    rescue SignalException, SystemExit, Interrupt => e
-        l.logs "Finalizing... "
-        BlackStack::OpenAIScraper.finalize
-        l.logf "done".green
-
-        l.log 'Bye!'
-        exit(0)
-    rescue => e
-        puts "Error: #{e.to_console.red}".red
-    end
-end
-
+# finish
 exit(0)
 
--- lib/openai-scraper.rb
+++ lib/openai-scraper.rb
@@ -3,6 +3,7 @@ require 'mechanize'
 require 'simple_cloud_logging'
 require "openai"
 require 'colorize'
+require "io/console"
 
 # require selenium
 require 'selenium-webdriver'
@@ -34,11 +35,11 @@ module BlackStack
 
         def self.init
             @@client = OpenAI::Client.new(access_token: OPENAI_APIKEY)
-            @@browser = Selenium::WebDriver.for :chrome
+#            @@browser = Selenium::WebDriver.for :chrome
         end            
 
         def self.finalize
-            @@browser.quit
+#            @@browser.quit
         end
 
         # help shown in the console
@@ -102,12 +103,7 @@ List of Commands:\n
             return response.dig("choices", 0, "message", "content")         
         end
 
-        # doanload the web page, and extract all links.
-        # reutrn an openai prompt sharing all links in a json structure for further reference.
-        #
-        # example: \wl https://leadhype.com/
-        # example: What is the contact page in this list of links \wl https://leadhype.com/
-        # example: Give me all the links in this URL: https://leadhype.com/
+        # download the web page, and extract all links.
         #
         def self.wl(url)
             # visit the url
@@ -136,6 +132,144 @@ List of Commands:\n
             #"I have the links in a webpage. Which one of these links is the link to the \"contact us\" page of the company? \n #{h.join("\n")}"
             h
         end # def wl
+
+        # download the web page, and extract the text.
+        #
+        def self.wt(url)
+            # visit the url
+            @@browser.navigate.to url
+            
+            # wait up to 30 seconds for the page to load
+            #wait = Selenium::WebDriver::Wait.new(:timeout => 30)
+            #wait.until { @@browser.execute_script("return document.readyState") == "complete" }
+
+            # wait up to 30 seconds for all ajax calls have been executed
+            #wait = Selenium::WebDriver::Wait.new(:timeout => 30)
+            #wait.until { @@browser.execute_script("return jQuery.active") == 0 }
+
+            # return the text of the webpage
+            @@browser.find_element(:tag_name, 'body').text
+        end # def wt
+
+        # show the promt and wait for the user input
+        def self.console
+            @@HISTORY = []
+
+            while true
+                prompt = nil
+                print "#{BlackStack::OpenAIScraper::PROMPT}> ".blue
+                
+                # get the user input, char by char
+                s = ''
+                i = 0 
+                while true
+                    c = $stdin.getch
+                    # if the user press enter, then break the loop
+                    if c == "\n" || c == "\r"
+                        puts
+                        @@HISTORY << s
+                        i = 0
+                        break
+                    # if the user press backspace, then remove the last char from the string
+                    elsif c == "\u007F"
+                        if i >= 1
+                            i -= 1
+                            s = s[0..-2]
+                            print "\b \b"
+                        end
+                    # if the user press ctrl+c, then exit
+                    elsif c == "\u0003"
+                        puts
+                        exit(0)
+                    # if the user press arrow-up
+                    elsif c == "\e"
+                        d = [$stdin.getch, $stdin.getch].join
+                        if d == "[A" && @@HISTORY.size > 0
+                            # remove the current prompt
+                            print "\b \b" * s.size
+                            # take the last prompt from the history
+                            s = @@HISTORY[-1]
+                            # remove the last promt from the history
+                            @@HISTORY = @@HISTORY[0..-2]
+                            # insert the prompt as the first in the history
+                            @@HISTORY.insert(0, s)
+                            # print the prompt
+                            i = s.size
+                            print s.strip
+                        elsif d == "[B" && @@HISTORY.size > 0
+                            # remove the current prompt
+                            print "\b \b" * s.size
+                            # take the first prompt from the history
+                            s = @@HISTORY[0]
+                            # remove the first promt from the history
+                            @@HISTORY = @@HISTORY[1..-1]
+                            # insert the prompt as the last in the history
+                            @@HISTORY.insert(-1, s)
+                            # print the prompt
+                            i = s.size
+                            print s.strip
+                        end
+                    else
+                        s += c
+                        i += 1
+                        print c
+                    end                        
+                end # while true
+                
+                begin
+                    # `\q` to quit
+                    if s == '\q'
+                        exit(0)
+                    # `\h` for help
+                    elsif s == '\h'
+                        puts BlackStack::OpenAIScraper.help 
+                        next
+                    # \wl <url>: download the web-page and pass the list of links to the model for further reference.\n
+                    elsif s =~ /\\wl/
+                        # find the url after the `\wl`, when \wl may be at any position into the string
+                        prompt = s
+                        i = 0
+                        s.split(' ').each { |x|
+                            if x == '\wl'
+                                url = s.split(' ')[i+1]
+                                prompt.gsub!(/\\wl #{url}/, BlackStack::OpenAIScraper.wl(url).to_s)
+                            end
+                            i += 1
+                        }
+                        puts BlackStack::OpenAIScraper::HINT1
+                        puts prompt.blue
+                    # \wt <url>: download the web-page and pass the text content to the model for further reference.\n 
+                    elsif s =~ /\\wt/
+                        # find the url after the `\wl`, when \wl may be at any position into the string
+                        prompt = s
+                        i = 0
+                        s.split(' ').each { |x|
+                            if x == '\wt'
+                                url = s.split(' ')[i+1]
+                                prompt.gsub!(/\\wt #{url}/, BlackStack::OpenAIScraper.wt(url).to_s)
+                            end
+                            i += 1
+                        }
+                        puts BlackStack::OpenAIScraper::HINT1
+                        puts prompt.blue
+                    else 
+                        prompt = s
+                    end
+                    # standard openai prompt
+                    puts BlackStack::OpenAIScraper.response(prompt).to_s.green
+            
+                rescue SignalException, SystemExit, Interrupt => e
+                    l.logs "Finalizing... "
+                    BlackStack::OpenAIScraper.finalize
+                    l.logf "done".green
+            
+                    l.log 'Bye!'
+                    exit(0)
+                rescue => e
+                    puts "Error: #{e.to_console.red}".red
+                end # begin
+            end # while true
+        end # def console            
 
     end # module OpenAIScraper
 end # module BlackStack
